5d28ccb1a2d226d1471039efb353c2cbeaf6f715
cursedcadaver <collinschacha@hotmail.com>
2024-07-15 17:53:59 +0300

    get user info from clerk

---
+++ app/profile/page.tsx
@@ -0,0 +1,24 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export default async function Page() {
+
+    // Get the userId from auth() -- if null, the user is not signed in
+    const { userId } = auth();
+
+    if (userId) {
+        console.log(userId);
+        // Query DB for user specific information or display assets only to signed in users
+    }
+
+    // Get the Backend API User object when you need access to the user's information
+    const user = await currentUser()
+    console.log(user?.username);
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            hello world
+        </main>
+    );
+
+    // Use `user` to render user details or create UI elements
+}
