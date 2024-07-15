944a96213b600e65aee4b4d86a0ae4369d7319fe
cursedcadaver <collinschacha@hotmail.com>
2024-07-15 15:38:59 +0300

    added new styles and navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,36 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { Navbar } from "@/components/navbar";
+// This is the root layout component for your Next.js app.
+// Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
+import { Bricolage_Grotesque } from 'next/font/google'
+import { Space_Mono } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import './globals.css'
+import { Navbar } from '@/components/navbar'
 
-const inter = Inter({ subsets: ["latin"] });
+const fontHeading = Bricolage_Grotesque({
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-heading',
+})
 
-export const metadata: Metadata = {
-  title: "Chirp",
-  description: "A Twitter clone",
-};
+const fontBody = Space_Mono({
+    subsets: ['latin'],
+    weight: ['400', '700'],
+    display: 'swap',
+    variable: '--font-body',
+})
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-     
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body
+                className={cn(
+                    'antialiased',
+                    fontHeading.variable,
+                    fontBody.variable
+                )}
+            ><Navbar />
+                {children}
+            </body>
+        </html>
+    )
 }
