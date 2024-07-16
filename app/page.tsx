dd8e71e22809ff5089c833f38f569bcce292c85a
cursedcadaver <collinschacha@hotmail.com>
2024-07-16 19:34:31 +0300

    created home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
 
+import { Card } from "@/components/card";
+import prisma from "@/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
-export default function Home() {
+const getSingleData = async () => {
+
+    try {
+        const data = await prisma.post.findMany({
+            orderBy: {
+                //most recent first
+                createdAt: "desc"
+            }
+        })
+
+        return data;
+    } catch (err) {
+        console.log(err)
+
+    }
+
+}
+
+
+export default async function Home() {
+    const data = await getSingleData()
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            hello world
-        </main>
+        <>
+            <div className="flex flex-col justify-center gap-0 align-middle">
+                <SignedIn>
+
+                    {data.map((post) => <Card key={post.id} author={post.author} content={post.content} Id={post.id} />)}
+                </SignedIn>
+                <SignedOut>
+                    <h1 className="text-3xl font-bold text-center">Sign in to view your chirps</h1>
+                </SignedOut>
+            </div>
+        </>
     );
 }
